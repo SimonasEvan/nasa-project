31b0c9a01dd1540c2ddcc646afdcd6110b0ec7a7
Simon Evan <simonas.evan26@gmail.com>
2023-02-22 14:43:57 +0200

    Updating packages & mongo.js

--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -1,19 +1,27 @@
 const mongoose = require('mongoose');
 
+require('dotenv').config();
+
+// Update below to match your own MongoDB connection string.
 const MONGO_URL = process.env.MONGO_URL;
 
 mongoose.connection.once('open', () => {
-    console.log(`MongoDB connection ready!`);
+  console.log('MongoDB connection ready!');
 });
 
 mongoose.connection.on('error', (err) => {
-    console.error(`Error: ${err}`);
+  console.error(err);
 });
 
 async function mongoConnect() {
-    await mongoose.connect(MONGO_URL);
+  await mongoose.connect(MONGO_URL);
+}
+
+async function mongoDisconnect() {
+  await mongoose.disconnect();
 }
 
 module.exports = {
-    mongoConnect,
+  mongoConnect,
+  mongoDisconnect,
 }
